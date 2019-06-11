e29f83b74a545cff7001701da6b7747a158e3e91
Elise Luitgaarden <elise.luitgaarden@gmail.com>
2019-06-11 14:45:39 +0200

    tostirng

--- Catan/GameFolder/Resource.cs
+++ Catan/GameFolder/Resource.cs
@@ -8,14 +8,17 @@ namespace Catan
 {
     public class Resource
     {
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
         public Resource(string name)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
         }
 
-
+        public override string ToString()
+        {
+            return Name;
+        }
 
     }
 }
